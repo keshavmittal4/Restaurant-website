1f575d149227db89841d5569cb03faacfd9ff08a
keshavmittal <85719153+keshavmittal4@users.noreply.github.com>
2024-09-04 09:49:51 +0530

    Update register.php

--- register.php
+++ register.php
@@ -2,10 +2,7 @@
 	$username = $_POST['username'];
 	$email = $_POST['email'];
 	$password = $_POST['password'];
-	// $gender = $_POST['gender'];
-	// $roll_num = $_POST['roll_num'];
-	// $Course = $_POST['Course'];
-	// $number = $_POST['number'];
+	
 
 	// Database connection
 	$conn = new mysqli('localhost','root','','restaurant');
